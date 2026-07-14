8ea4fcaa046f2bbd5401ad57da7f19fcfbcc1792
Sean Murphy <sean@iamseanmurphy.com>
2026-07-14 14:12:27 +0000

    22.2

--- conceptly/functions.php
+++ conceptly/functions.php
@@ -5,7 +5,7 @@ function conceptly_setup() {
 	/**
 	 * Define Theme Version
 	 */
-	define( 'CONCEPTLY_THEME_VERSION', '22.1' );
+	define( 'CONCEPTLY_THEME_VERSION', '22.2' );
 	
 	/*
 	 * Make theme available for translation.
